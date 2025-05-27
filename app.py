7df9429899e9faf4a7ed25b734952fc78410fbeb
silvikusuma04 <silvikusuma043@gmail.com>
2025-05-27 23:15:05 +0700

    update

--- app.py
+++ app.py
@@ -128,7 +128,3 @@ class ChatEndpoint(Resource):
 # Register namespace with base path /api
 api.add_namespace(chat_ns, path='/api')
 
-# Run Flask app
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host='0.0.0.0',port=port)
